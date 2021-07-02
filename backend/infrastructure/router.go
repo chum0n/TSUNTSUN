27fb57d56e58eede25bc271d1fc790471a5c5ed0
chum0n <cyumon_tkt.0914@icloud.com>
2021-07-03 04:16:16 +0900

    add log format

--- backend/infrastructure/router.go
+++ backend/infrastructure/router.go
@@ -3,8 +3,10 @@ package infrastructure
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/yot-sailing/TSUNTSUN/interfaces/controllers"
 )
 
@@ -13,6 +15,14 @@ func Init() {
 	e.HideBanner = true
 	e.HidePort = true
 	userController := controllers.NewUserController(NewSqlHandler())
+
+	// Middleware
+	logger := middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: logFormat(),
+		Output: os.Stdout,
+	})
+	e.Use(logger)
+	e.Use(middleware.Recover())
 
 	// 接続テスト
 	e.GET("/api/test", func(c echo.Context) error {
@@ -40,4 +50,29 @@ func Init() {
 
 	// start server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func logFormat() string {
+	// Refer to https://github.com/tkuchiki/alp
+	var format string
+	format += "time:${time_rfc3339}\t"
+	format += "host:${remote_ip}\t"
+	format += "forwardedfor:${header:x-forwarded-for}\t"
+	format += "req:-\t"
+	format += "status:${status}\t"
+	format += "method:${method}\t"
+	format += "uri:${uri}\t"
+	format += "size:${bytes_out}\t"
+	format += "referer:${referer}\t"
+	format += "ua:${user_agent}\t"
+	format += "reqtime_ns:${latency}\t"
+	format += "cache:-\t"
+	format += "runtime:-\t"
+	format += "apptime:-\t"
+	format += "vhost:${host}\t"
+	format += "reqtime_human:${latency_human}\t"
+	format += "x-request-id:${id}\t"
+	format += "host:${host}\n"
+
+	return format
 }
