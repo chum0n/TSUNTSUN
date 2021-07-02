51901e08d0c71c1c893ae9cf142a5a4b2f1464b5
chum0n <cyumon_tkt.0914@icloud.com>
2021-07-03 04:15:12 +0900

    get users

--- backend/infrastructure/router.go
+++ backend/infrastructure/router.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -9,25 +10,29 @@ import (
 
 func Init() {
 	e := echo.New()
+	e.HideBanner = true
+	e.HidePort = true
 	userController := controllers.NewUserController(NewSqlHandler())
 
 	// 接続テスト
 	e.GET("/api/test", func(c echo.Context) error {
+		fmt.Println("aa")
 		return c.String(http.StatusOK, "This is test!")
 	})
 
-	e.GET("/users", func(c echo.Context) error {
+	e.GET("/api/users", func(c echo.Context) error {
+		fmt.Println("aa")
 		users := userController.GetUser()
 		c.Bind(&users)
 		return c.JSON(http.StatusOK, users)
 	})
 
-	e.POST("/users", func(c echo.Context) error {
+	e.POST("/api/users", func(c echo.Context) error {
 		userController.Create(c)
 		return c.String(http.StatusOK, "created")
 	})
 
-	e.DELETE("/users/:id", func(c echo.Context) error {
+	e.DELETE("/api/users/:id", func(c echo.Context) error {
 		id := c.Param("id")
 		userController.Delete(id)
 		return c.String(http.StatusOK, "deleted")
