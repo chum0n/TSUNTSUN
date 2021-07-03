0c6dee1e0a2b07e3c7b58684635fd700ebd586ab
admin <admin@PC-A077.local>
2021-07-03 17:13:49 +0900

    modify spell miss

--- backend/domain/tsundoku.go
+++ backend/domain/tsundoku.go
@@ -10,6 +10,6 @@ type Tsundoku struct {
 	Author      string    `json:"author"`
 	URL         string    `json:"url"`
 	Deadline    time.Time `json:"deadline"`
-	ReqiredTime string    `json:"requiredTime"`
+	RequiredTime string   `json:"requiredTime"`
 	CreatedAt   time.Time `json:"createdAt"`
 }
