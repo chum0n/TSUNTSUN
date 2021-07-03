492592aa43f59d520d9ae577396db2f402518224
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-07-04 00:04:01 +0900

    add:login button

--- frontend/tsuntsun/src/component/header.tsx
+++ frontend/tsuntsun/src/component/header.tsx
@@ -6,7 +6,22 @@ function Header() {
       <Logo href="." className="brand">
         ロゴ TsunTsun
       </Logo>
-      <div>チュモンさん、こんにちは　設定 ログアウト</div>
+      <div>
+        チュモンさん、こんにちは　設定
+        <img
+          src={`${process.env.PUBLIC_URL}/LineLoginButtonImage/images/DeskTop/1x/20dp/btn_login_base.png`}
+          alt="lINEでログイン"
+          onClick={(e) =>
+            (window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${
+              process.env.REACT_APP_CHANNEL_ID
+            }&redirect_uri=https://tsuntsun.herokuapp.com&state=${Math.random()
+              .toString(32)
+              .substring(2)}&scope=profile%20openid&nonce=${Math.random()
+              .toString(32)
+              .substring(2)}&bot_prompt=aggressive`)
+          }
+        ></img>
+      </div>
     </HeaderBase>
   );
 }
