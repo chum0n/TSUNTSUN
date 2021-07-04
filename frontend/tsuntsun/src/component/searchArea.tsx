7b871ff3c1409235289255292adec2ffcc8e6cf8
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-07-04 12:50:27 +0900

    Revert "add:search tag"
    This reverts commit f41d5e31d924ed24750f96f7741e0cd4ea80a021.

--- frontend/tsuntsun/src/component/searchArea.tsx
+++ frontend/tsuntsun/src/component/searchArea.tsx
@@ -1,66 +1,29 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { DarkButton } from "./button";
-import { TagObject } from "./tsumi";
 
 function SearchArea() {
-  const [radio, setRadio] = useState<string>("全て");
-  const [tags, setTags] = useState<TagObject[]>([]);
-
-  useEffect(() => {
-    axios
-      .get("https://tsuntsun-api.herokuapp.com/api/users/1/tags")
-      .then((res) => {
-        setTags(res.data);
-      });
-  }, []);
   return (
     <Area className="search-area">
-      <OneSearchArea>
-        <Label>種類</Label>
-        <input
-          type="radio"
-          name="cat"
-          value="全て"
-          checked={radio === "全て"}
-          onChange={() => setRadio("全て")}
-        />
+      <div>
+        種類
+        <input type="radio" name="category" value="全て" />
         全て
-        <input
-          type="radio"
-          name="cat"
-          value="本のみ"
-          checked={radio === "本のみ"}
-          onChange={() => setRadio("本のみ")}
-        />
+        <input type="radio" name="category" value="本のみ" />
         本のみ
-        <input
-          type="radio"
-          name="cat"
-          value="サイトのみ"
-          checked={radio === "サイトのみ"}
-          onChange={() => setRadio("サイトのみ")}
-        />
+        <input type="radio" name="category" value="サイトのみ" />
         サイトのみ
-      </OneSearchArea>
-
-      <OneSearchArea>
-        <Label>タグ</Label>
+      </div>
+      <div>
+        タグ
         <select name="tag">
-          <option value="" disabled></option>
-          {tags.map((t) => (
-            <option value={t.id}>{t.name}</option>
-          ))}
+          <option value="サンプル1">サンプル1</option>
+          <option value="サンプル2">サンプル2</option>
+          <option value="サンプル3">サンプル3</option>
         </select>
-      </OneSearchArea>
-      <OneSearchArea>
-        <Label>キーワード</Label>
-        <input type="search"></input>
-      </OneSearchArea>
-      <ButtonWrap>
-        <DarkButton onClick={() => {}}>検索</DarkButton>
-      </ButtonWrap>
+      </div>
+      <div>
+        キーワード <input type="search"></input>
+      </div>
+      <button>検索</button>
     </Area>
   );
 }
@@ -70,19 +33,5 @@ export default SearchArea;
 const Area = styled.div`
   background: #eaf5d3;
   padding: 16px;
-  padding-left: 48px;
   text-align: left;
-`;
-
-const OneSearchArea = styled.div`
-  margin: 5px 0;
-`;
-
-const ButtonWrap = styled.div`
-  text-align: center;
-`;
-
-const Label = styled.label`
-  display: inline-block;
-  width: 100px;
 `;
