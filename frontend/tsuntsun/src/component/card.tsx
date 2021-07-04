eb41e052325a93b3b2da56a1192c723e50aa9cb5
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-07-04 12:51:14 +0900

    Revert "add: pagenation"
    This reverts commit b9c571c86e8b833645f20595d818b86170c9e372.

--- frontend/tsuntsun/src/component/card.tsx
+++ frontend/tsuntsun/src/component/card.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Tsumi, { TsumiObject } from "./tsumi";
 
@@ -6,13 +5,6 @@ const Card: React.FC<{ name: string; tsumis: TsumiObject[] }> = ({
   name,
   tsumis,
 }) => {
-  const [page, setPage] = useState<number>(1);
-  const [isLastPage, setIsLastPage] = useState<boolean>(tsumis.length <= 10);
-
-  useEffect(() => {
-    setIsLastPage(page * 10 > tsumis.length);
-  }, [page, tsumis]);
-
   const list: JSX.Element[] = [];
   tsumis.forEach((tsumi) => {
     list.push(<Tsumi key={tsumi.id} {...tsumi}></Tsumi>);
@@ -20,30 +12,10 @@ const Card: React.FC<{ name: string; tsumis: TsumiObject[] }> = ({
   return (
     <div>
       <h2>{name}</h2>
-      {tsumis.length !== 0 ? (
-        <div>
-          <div>
-            {isLastPage ? tsumis.length : page * 10}/
-            <NumBig>{tsumis.length}</NumBig>件
-          </div>
-          {list}
-          <div>
-            {page !== 1 && (
-              <PagenationButton onClick={() => setPage((p) => p - 1)}>
-                {"<"}
-              </PagenationButton>
-            )}
-            {page}
-            {!isLastPage && (
-              <PagenationButton onClick={() => setPage((p) => p + 1)}>
-                {">"}
-              </PagenationButton>
-            )}
-          </div>
-        </div>
-      ) : (
-        <div>なし</div>
-      )}
+      <div>
+        10/<NumBig>{tsumis.length}</NumBig>件
+      </div>
+      {list}
     </div>
   );
 };
@@ -54,13 +26,4 @@ const NumBig = styled.span`
   font-size: 24px;
   font-weight: 400;
   line-height: 28px;
-`;
-
-const PagenationButton = styled.button`
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  :hover {
-    color: #9ab85d;
-  }
 `;
