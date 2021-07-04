4790bc05e3fcfe5e83b2cad4c01f7d50756b387a
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-07-04 14:11:29 +0900

    fix

--- frontend/tsuntsun/src/pages/main.tsx
+++ frontend/tsuntsun/src/pages/main.tsx
@@ -13,13 +13,30 @@ function Main() {
   const query = new URLSearchParams(search);
 
   useEffect(() => {
-    const bodyFormData = new FormData();
-    const code = query.get("code");
-    bodyFormData.append("id_token", code !== null ? code : "");
-    bodyFormData.append("access_token", code !== null ? code : "");
+    const data = {
+      code: query.get("code") ? query.get("code") : "",
+      redirect_uri: "https://tsuntsun.herokuapp.com",
+      client_id: process.env.REACT_APP_CHANNEL_ID,
+      client_secret: process.env.REACT_APP_CHANNEL_SECRET,
+    };
     axios
-      .post("https://tsuntsun-api.herokuapp.com/api/line_login", bodyFormData)
-      .then((res) => {});
+      .post("https://api.line.me/oauth2/v2.1/token", data, {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
+      .then((res) => {
+        console.log(res);
+        const bodyFormData = new FormData();
+        bodyFormData.append("id_token", res.data.id_token);
+        bodyFormData.append("access_token", res.data.access_token);
+        axios
+          .post(
+            "https://tsuntsun-api.herokuapp.com/api/line_login",
+            bodyFormData
+          )
+          .then((res) => {
+            console.log(res);
+          });
+      });
   }, []);
   return (
     <Body>
