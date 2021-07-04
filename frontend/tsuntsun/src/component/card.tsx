b9c571c86e8b833645f20595d818b86170c9e372
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-07-04 09:24:32 +0900

    add: pagenation

--- frontend/tsuntsun/src/component/card.tsx
+++ frontend/tsuntsun/src/component/card.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Tsumi, { TsumiObject } from "./tsumi";
 
@@ -5,6 +6,13 @@ const Card: React.FC<{ name: string; tsumis: TsumiObject[] }> = ({
   name,
   tsumis,
 }) => {
+  const [page, setPage] = useState<number>(1);
+  const [isLastPage, setIsLastPage] = useState<boolean>(tsumis.length <= 10);
+
+  useEffect(() => {
+    setIsLastPage(page * 10 > tsumis.length);
+  }, [page, tsumis]);
+
   const list: JSX.Element[] = [];
   tsumis.forEach((tsumi) => {
     list.push(<Tsumi key={tsumi.id} {...tsumi}></Tsumi>);
@@ -12,10 +20,30 @@ const Card: React.FC<{ name: string; tsumis: TsumiObject[] }> = ({
   return (
     <div>
       <h2>{name}</h2>
-      <div>
-        10/<NumBig>{tsumis.length}</NumBig>件
-      </div>
-      {list}
+      {tsumis.length !== 0 ? (
+        <div>
+          <div>
+            {isLastPage ? tsumis.length : page * 10}/
+            <NumBig>{tsumis.length}</NumBig>件
+          </div>
+          {list}
+          <div>
+            {page !== 1 && (
+              <PagenationButton onClick={() => setPage((p) => p - 1)}>
+                {"<"}
+              </PagenationButton>
+            )}
+            {page}
+            {!isLastPage && (
+              <PagenationButton onClick={() => setPage((p) => p + 1)}>
+                {">"}
+              </PagenationButton>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div>なし</div>
+      )}
     </div>
   );
 };
@@ -26,4 +54,13 @@ const NumBig = styled.span`
   font-size: 24px;
   font-weight: 400;
   line-height: 28px;
+`;
+
+const PagenationButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  :hover {
+    color: #9ab85d;
+  }
 `;
