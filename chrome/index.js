291794b9cb1c00c0607ac5ba07fdb33abda3c391
mimimimizuki <44560105+mimimimizuki@users.noreply.github.com>
2021-09-20 01:50:25 +0900

    line login need specified redirect uri

--- chrome/index.js
+++ chrome/index.js
@@ -1,15 +1,15 @@
+'use strict'
+let Data = {"Title": "", "URL": ""}
+chrome.tabs.query({active: true, lastFocusedWindow:true}, tabs => {
+  Data.URL = tabs[0].url;
+  Data.Title = tabs[0].title
+});
+var added = false;
 $(document).ready(function () {
-  var url = "";
-  chrome.tabs.query(
-    {
-      active: true,
-      lastFocusedWindow: true,
-    },
-    function (tabs) {
-      $("input#title").val(tabs[0].title);
-      url = tabs[0].url;
-    }
-  );
+  $("input#title").val(Data.Title);
+  $("input#url").val(Data.URL);
+
+  localStorage.setItem("pre_url", Data.URL);
 
   $("button.add").on("click", () => {
     const newTag = $("input#new-tag").val();
@@ -25,12 +25,34 @@ $(document).ready(function () {
   });
 
   $("button.save").on("click", () => {
-    $(".add-view").hide();
+    // $(".add-view").hide();
     const accessToken = localStorage.getItem("accessToken");
-    if (accessToken == null) {
-      console.log("need to line login")
+    if (accessToken == null) { // not login 
+      console.log("need to line login");
+      const login_url = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1656173858&redirect_uri=https://tsuntsun.herokuapp.com&state="+Math.random().toString(32).substring(2)+"&scope=profile%20openid&nonce="+Math.random().toString(32).substring(2)+"&bot_prompt=aggressive"
 
-    } else {
+      chrome.tabs.query({'active': true}, function(tabs) {
+        chrome.tabs.update(tabs[0].id, {url: login_url } )
+        console.log("login success")
+        $(".login-button").on("click", () => {
+          console.log("clicked")
+        })
+      });
+
+      // const data = {"grant_type":'authorization_code', "code":"", "redirect_uil":Data.URL, "client_id": 1656173858, "client_secret":"e07ff58ce5052f7a5698319b3ec8493c"};
+      // const param  = {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json; charset=utf-8",
+      //   },
+      //   // リクエストボディ
+      //   body: JSON.stringify(data)
+      // };
+      // fetch('https://api.line.me/oauth2/v2.1/token', param).then(token => {
+      //   console.log(token);
+      //   localStorage.setItem("access_token", token);
+      // })
+    } else { //loginしていたらTODOここの動作の確認
       const data = {URL:url, Title:$("input#title").val(), Tags:$("tags").val()};
       const param  = {
         method: "POST",
@@ -47,6 +69,7 @@ $(document).ready(function () {
         .then((res)=>{
           console.log( res.json() );
           $(".done-view").show();
+          $(".add-view").hide();
         }).catch((err)=>{
           console.log(err);
           $(".done-view").show();
@@ -55,4 +78,3 @@ $(document).ready(function () {
     }
     
   });
-});
