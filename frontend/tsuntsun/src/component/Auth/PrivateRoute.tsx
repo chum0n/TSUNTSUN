52dfe0ceaabf74704d37b6105bed999dec6a840a
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-22 22:13:21 +0900

    update route

--- frontend/tsuntsun/src/component/Auth/PrivateRoute.tsx
+++ frontend/tsuntsun/src/component/Auth/PrivateRoute.tsx
@@ -5,27 +5,33 @@ import { useState } from "react";
 
 const PrivateRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
   const auth = useAuth();
+
   useEffect(() => {
     const f = async () => {
       setIsLoggedIn(await auth.isLoggedIn());
-      console.log(await auth.isLoggedIn());
+      setIsLoaded(true);
+      console.log("await isLoggedIn", await auth.isLoggedIn());
     };
     f();
-  }, []);
+  }, [auth]);
+
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        isLoggedIn ? (
+        isLoaded && isLoggedIn ? (
           children
-        ) : (
+        ) : isLoaded ? (
           <Redirect
             to={{
               pathname: "/login",
               state: { from: location },
             }}
           />
+        ) : (
+          <p>読み込み中</p>
         )
       }
     />
