2d468fb5dcbfca8ddba35768f68114a654e3f477
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-24 01:08:26 +0900

    use default axios

---
+++ frontend/tsuntsun/src/utils/defaultAxios.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { accessToken } from "../contexts/AuthContext";
+
+const createAxiosInstance = () => {
+  const axiosInstance = axios.create({
+    baseURL: "https://tsuntsun-api.herokuapp.com/api/",
+  });
+
+  axiosInstance.interceptors.request.use((request) => {
+    request.headers["Authorization"] = accessToken();
+    console.dir(request);
+    return request;
+  });
+
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      console.log(error);
+    }
+  );
+  return axiosInstance;
+};
+
+const defaultAxios = createAxiosInstance();
+
+export default defaultAxios;
