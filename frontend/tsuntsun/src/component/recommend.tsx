5cea60d2baa445942d4e75dfb7696f39cf76d5cd
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-24 18:05:06 +0900

    reccomend area

--- frontend/tsuntsun/src/component/recommend.tsx
+++ frontend/tsuntsun/src/component/recommend.tsx
@@ -1,8 +1,9 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import defaultAxios from "../utils/defaultAxios";
+import Color from "../const/Color";
 import { TsumiObject } from "./tsumi";
+import { RiBookFill, RiComputerLine } from "react-icons/ri";
 
 function Recommend() {
   const today = new Date();
@@ -20,34 +21,46 @@ function Recommend() {
   }, []);
   return (
     <RecommendBox className="recommend">
-      <h2>今日のおすすめ</h2>
-      <OneRecommend>
-        {<a href={site?.url}>{site?.title}</a>}　……　{site?.requiredTime}
-        で読める！
-      </OneRecommend>
-      <OneRecommend>
-        {book?.title}　……　
-        {book?.deadline && (
-          <>
-            {Math.floor(
-              (Date.parse(book?.deadline) - today.getTime()) / 86400000
-            ) >= 0
-              ? "あと"
-              : ""}
-            {Math.abs(
-              Math.floor(
+      <TitleArea>
+        <h3>今日のおすすめ</h3>
+      </TitleArea>
+      <BodyArea>
+        <OneRecommend>
+          <RiBookFill />
+          {<a href={site?.url}>{site?.title}</a>}
+          {site?.requiredTime && (
+            <>
+              　……　{site?.requiredTime}
+              で読める！
+            </>
+          )}
+        </OneRecommend>
+        <OneRecommend>
+          <RiComputerLine />
+          {book?.title}
+          {book?.deadline && (
+            <>
+              　……　
+              {Math.floor(
                 (Date.parse(book?.deadline) - today.getTime()) / 86400000
-              )
-            )}
-            日
-            {Math.floor(
-              (Date.parse(book?.deadline) - today.getTime()) / 86400000
-            ) < 0
-              ? "経過"
-              : ""}
-          </>
-        )}
-      </OneRecommend>
+              ) >= 0
+                ? "あと"
+                : ""}
+              {Math.abs(
+                Math.floor(
+                  (Date.parse(book?.deadline) - today.getTime()) / 86400000
+                )
+              )}
+              日
+              {Math.floor(
+                (Date.parse(book?.deadline) - today.getTime()) / 86400000
+              ) < 0
+                ? "経過"
+                : ""}
+            </>
+          )}
+        </OneRecommend>
+      </BodyArea>
     </RecommendBox>
   );
 }
@@ -56,15 +69,33 @@ export default Recommend;
 
 const RecommendBox = styled.div`
   box-sizing: border-box;
+  display: flex;
+  width: 100%;
+  height: 120px;
+  margin: 16px auto;
+  text-align: left;
+  border-radius: 10px;
+`;
+
+const TitleArea = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 32px;
+  white-space: nowrap;
+  background-color: ${Color.PRIMARY};
+  border-radius: 10px 0 0 10px;
+`;
+const BodyArea = styled.div`
+  width: 100%;
   padding-right: 16px;
   padding-bottom: 16px;
   padding-left: 16px;
-  margin: 16px auto;
-  text-align: left;
-  border: 6px dashed #9ab85d;
+  background-color: white;
+  border-radius: 0 10px 10px 0;
 `;
 
 const OneRecommend = styled.div`
+  height: 50%;
   margin: 8px 0;
   font-size: large;
 `;
